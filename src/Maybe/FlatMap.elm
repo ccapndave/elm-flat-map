6878b8b095a3b3b59321e701a25c086331863ba7
Dave Keen <dev@ruffness.com>
2018-12-04 10:55:13 +0100

    Added flatMap for Result

--- src/Maybe/FlatMap.elm
+++ src/Maybe/FlatMap.elm
@@ -17,7 +17,15 @@ For example:
 
 -}
 
-import Maybe.Extra exposing (join)
+
+join : Maybe (Maybe a) -> Maybe a
+join mx =
+    case mx of
+        Just x ->
+            x
+
+        Nothing ->
+            Nothing
 
 
 {-| -}
